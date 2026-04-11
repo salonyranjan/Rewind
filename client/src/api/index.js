e1241e5f882c622954ae56fb3578f6d7e93f8290
Salony Ranjan <salonyranjan@gmail.com>
2026-04-12 01:41:51 +0530

    fix: update api url for production deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// Ensure this matches your server port
-const url = 'http://localhost:5000/posts';
+const url = '/api/posts'; 
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
