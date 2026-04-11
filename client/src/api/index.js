45d06d50bc18420e18231dc8ae353dcfd41bffe6
Salony Ranjan <salonyranjan@gmail.com>
2026-04-12 02:54:11 +0530

    cleanup: removed root configs for client-only vercel build

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = '/api/posts'; 
+const url = 'https://rewind-api-alwp.onrender.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
